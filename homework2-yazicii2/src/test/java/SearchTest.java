803bdca03120a2ae100bf32fed6e671aa38f00be
Oğuzhan Yazıcı <101544408+yazicii@users.noreply.github.com>
2022-07-02 21:47:41 +0300

    Update SearchTest.java

--- homework2-yazicii2/src/test/java/SearchTest.java
+++ homework2-yazicii2/src/test/java/SearchTest.java
@@ -19,7 +19,7 @@ public class SearchTest extends BaseTest{
         WebElement searchBox = driver.findElement(By.id("searchBox"));                     //search butonunun lacater'ı
         searchBox.click();                                                                 //search butonuna tıklar
         Assert.assertTrue(searchBox.isDisplayed());                                        //Assert isDisplayed() search butonunun görünür olup olmadığını döner
-        searchBox.sendKeys("Cierra");                                           //search butonun da cier kelimesini yazar ve arar.
+        searchBox.sendKeys("Cierra");                                                      //search butonun da cier kelimesini yazar ve arar.
         WebElement searchedWord = driver.findElement(By.className("rt-tbody"));            //aranan kelime için gelecek sonuçların bulunacağı bölümün locater'ı
         Assert.assertTrue(searchedWord.isDisplayed());                                     //aranan kelime ile bulunan kelimenin içerisinde göründüğünü test eder.
 
